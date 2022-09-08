58ef5e8c25066ca782cf16b8d3c855a50a4acdcc
tashim <tashim@rinna.co.jp>
2022-09-08 22:54:14 +0900

    fix: remove extra dir from index

---
+++ ASP.NetCore.WordShuffle/Pages/User.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NetCore.WordShuffle.Pages
+{
+    public class UserModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public UserModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
